cf9deea4d50873ad74f7b57de230705762347b23
Tiago Costa <tsdc12@gmail.com>
2025-12-18 22:32:36 +0100

    Initial Commit

---
+++ Refuge Worldwide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Refuge Worldwide
+//
+//  Created by Tiago Costa on 12/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Refuge Worldwide/Refuge_WorldwideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Refuge_WorldwideApp.swift
+//  Refuge Worldwide
+//
+//  Created by Tiago Costa on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Refuge_WorldwideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
